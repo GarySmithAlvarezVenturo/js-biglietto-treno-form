382380bc2a1901cdf7f7a42c4e593c06f9e99600
GarySmithAlvarezVenturo <Garyalvarez@outlook.it>
2023-04-13 23:39:00 +0200

    end project

---
+++ assets/script.js
@@ -0,0 +1,82 @@
+// Recive information like name of passenger, his age and distance of journey
+
+const passengerName = document.querySelector('#passengerName');
+const journeyDistance = document.querySelector('#journeyDistance');
+const passengerAge = document.querySelector('#passengerAge');
+
+// Get Ticket price
+const ratePerKM = 0.21;
+let ticketPrice = 0;
+
+
+
+
+let passengerNameShow = document.querySelector('#passengerNameShow');
+let ticketPriceShow = document.querySelector('#ticketPriceShow');
+let codeShow = document.querySelector('#codeShow')
+let cabinShow = document.querySelector('#cabinShow')
+
+
+
+
+const btn = document.querySelector("button"); 
+btn.addEventListener('click', function () {
+    
+    passengerNameShow.innerHTML = passengerName.value + ";";
+    let ticketSale = 0;
+    let ticketPriceFinal = 0;
+    ticketPrice = journeyDistance.value * ratePerKM;
+    if (passengerAge.value < 18) {
+        ticketSale = ticketPrice / 5;
+        ticketPriceFinal = ticketPrice - ticketSale;
+        ticketPriceShow.innerHTML = ticketPriceFinal + " €;";
+        saleShow.innerHTML = "Sconto minorenni;";
+    }
+    
+    else if (passengerAge.value >= 65) {
+        ticketSale = ticketPrice / 2.5;
+        ticketPriceFinal = ticketPrice - ticketSale;
+        ticketPriceShow.innerHTML = ticketPriceFinal + " €;";
+        saleShow.innerHTML = "Sconto Over 65;";
+    }
+    
+    else  {
+        ticketPriceFinal = ticketPrice - ticketSale;
+        ticketPriceShow.innerHTML = ticketPriceFinal + " €;";
+        saleShow.innerHTML = "Tariffa base;";
+    }
+
+    cabinShow.innerHTML = (Math.floor(Math.random()* 9) + 1)  + ";";
+    codeShow.innerHTML = (Math.floor(Math.random()* (99999 - 10000) + 10000))  + ";";  
+
+});
+
+
+/*
+// Input validator
+if (isNaN(trainDistance)) {
+    // console.log("Hai inserito un valore errato nella distanza. Aggiorna la pagina e riprova");
+    alert("Hai inserito un valore errato nella distanza. Aggiorna la pagina e riprova")
+} else {
+
+}
+
+if (isNaN(userAge)) {
+    // console.log("Hai inserito una età non valida. Aggiorna la pagina e riprova");
+    alert("Hai inserito una età non valida. Aggiorna la pagina e riprova")
+} else {
+    
+}
+
+
+
+document.getElementById("ticketSale").innerHTML = (Math.round(ticketSale * 100) / 100).toFixed(2) + " €";
+
+// Get final ticket price
+let finalPrice = ticketPrice - ticketSale;
+
+// console.log("Final price: " + finalPrice);
+
+document.getElementById("finalPrice").innerHTML = (Math.round(finalPrice * 100) / 100).toFixed(2) + " €";
+
+*/
